7ccb73e25461d84eb50b9e9ab4eef4505fe82768
Zaltana <dale@zaltana.io>
2020-06-14 15:48:58 -0400

    Updates

---
+++ precache-manifest.78279628005b1d16abd398784495048e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cfdbaf59d32aa61318a9f5d0c2b9b6c",
+    "url": "/reddit-world-recipes/index.html"
+  },
+  {
+    "revision": "cb4ed3128c149f2d04dc",
+    "url": "/reddit-world-recipes/static/css/main.69c8d43c.chunk.css"
+  },
+  {
+    "revision": "01f388d7db865c60b6ef",
+    "url": "/reddit-world-recipes/static/js/2.c5afcf63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reddit-world-recipes/static/js/2.c5afcf63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb4ed3128c149f2d04dc",
+    "url": "/reddit-world-recipes/static/js/main.dd37d567.chunk.js"
+  },
+  {
+    "revision": "114534bcc445f089d0d2",
+    "url": "/reddit-world-recipes/static/js/runtime-main.0f50b045.js"
+  }
+]);
